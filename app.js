093ef5d9db80dac01a1fe295658ec4d642c44e19
AbdullahAhmadAAK <abdullahaak06@gmail.com>
2024-09-12 20:06:19 +0500

    add authorization a bit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 const cors = require('cors');
 
-const authenticateToken = require('./middleware/auth')
+const { authenticateToken, authorizeAdmin } = require('./middleware/auth')
 
 const usersAuthRouter = require('./routes/users/auth')
 const adminJobsRouter = require('./routes/admin/jobs')
@@ -11,19 +11,15 @@ const usersJobsRouter = require('./routes/users/jobs')
 var express = require('express');
 var app = express();
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
-)
-
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
 app.locals.supabase = supabase;
 
 app.use(cors());
 app.use(express.json())   
 
 app.use('/auth/users', usersAuthRouter)
-app.use('/admin/jobs', authenticateToken, adminJobsRouter)
-app.use('/jobs', authenticateToken, usersJobsRouter)
+app.use('/admin/jobs', authenticateToken, authorizeAdmin, adminJobsRouter)
+app.use('/jobs', authenticateToken, authorizeAdmin, usersJobsRouter)
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,35 +1,72 @@
 const jwt = require('jsonwebtoken');
-const { renderSuccessResponse, renderErrorResponse } = require('../routes/helpers/supabase/generic/render-response-helpers');
+const { renderErrorResponse } = require('../routes/helpers/supabase/generic/render-response-helpers');
 
 // Replace 'your_secret_key' with your actual secret key used to sign the JWTs
 const secretKey = process.env.SUPABASE_JWT_SECRET;
 
 // Middleware to check if the token is valid
 function authenticateToken(req, res, next) {
-    // Get the token from the request header
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  // Get the token from the request header
 
-    if (token == null) {
-        // If there's no token, return 401 (Unauthorized)
-        // return res.sendStatus(401);
-        return renderErrorResponse(res, ['Unauthorized'], 401)
+  // console.log('headers r these: ', req.headers);
+  
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  // console.log('zis is token:', token);
+  
+  if (token == null) {
+    // If there's no token, return 401 (Unauthorized)
+    // return res.sendStatus(401);
+    return renderErrorResponse(res, ['Unauthorized'], 401)
+  }
+
+  // Verify the token
+  jwt.verify(token, secretKey, async (err, user) => {
+    if (err) {
+      // If the token is invalid, return 403 (Forbidden)
+      // return res.sendStatus(403);
+      return renderErrorResponse(res, ['Invalid token'], 403)
     }
 
-    // Verify the token
-    jwt.verify(token, secretKey, (err, user) => {
-        if (err) {
-            // If the token is invalid, return 403 (Forbidden)
-            // return res.sendStatus(403);
-            return renderErrorResponse(res, ['Invalid token'], 403)
-        }
+    // TODO: get from supabase
+    // console.log('lez try to get user role: ', user );
+    const supabase = req.app.locals.supabase
+    const userId = user.sub
 
-        // If the token is valid, attach the user object to the request
-        req.user = user;
+    const {data, error} = await supabase
+      .from('user_profiles')
+      .select('role:user_roles(slug)')
+      .eq('id', userId)
+      .single()
 
-        // Call the next middleware/route handler
-        next();
-    });
+    if (error) {
+      console.error(error)
+      return renderErrorResponse(res, ['Internal server error'], 500)
+    }
+
+    console.log('this is data of user i get from supabase: ', data);
+    
+    const role = data.role.slug
+
+    // If the token is valid, attach the user object to the request
+    req.user = {...user, role: role};
+
+    // Call the next middleware/route handler
+    next();
+  });
 }
 
-module.exports = authenticateToken;
+function authorizeAdmin(req, res, next) {
+  console.log('i came to auth admin');
+  
+  if (req.user.role !== 'admin' && req.user.role !== 'superadmin') {
+    return renderErrorResponse(res, ['Unauthorized'], 401)
+  }
+
+  else {
+    next()
+  }
+}
+
+module.exports = { authenticateToken, authorizeAdmin };
